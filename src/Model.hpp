8fb26dc49ed27dea90d715fc0d2ded8f9e17c99e
Caerind <charles.mailly@free.fr>
2024-02-07 10:44:50 +0100

    Update Model.hpp

--- src/Model.hpp
+++ src/Model.hpp
@@ -11,8 +11,15 @@ class Model
 public:
     virtual const char* GetName() const = 0;
 
+    virtual bool StartTraining() { m_IsTraining = true; return true; }
     virtual void Train(Simulation& simulation) = 0;
+    virtual bool StopTraining() { m_IsTraining = false; return false; }
+    bool IsTraining() const { return m_IsTraining; }
+
     virtual bool MakeBestNeuralNetwork(NeuralNetwork& neuralNetwork) = 0;
+
+private:
+    bool m_IsTraining{ false };
 };
 
 } // namespace BrainFramework
